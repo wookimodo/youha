4d3116886413546e3821144facead941918df80c
unknown <rudghk1437@naver.com>
2023-04-18 18:45:26 +0900

    youha data collecting

--- Youha/settings.py
+++ Youha/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import env_info
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-veq0tnmipj#ws462u9q82ljk9(7ma^mias4$oe=_6k@!og-e3q'
+SECRET_KEY = env_info.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env_info.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env_info.ALLOWED_HOSTS
 
 
 # Application definition
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'Youha.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,12 +77,7 @@ WSGI_APPLICATION = 'Youha.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = env_info.DATABASES
 
 
 # Password validation
@@ -103,19 +102,21 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+# USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ scripts/youha_crawling.py
@@ -0,0 +1,99 @@
+import time
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.common.exceptions import ElementNotInteractableException
+# from selenium.common.exceptions import StaleElementReferenceException
+# from selenium.webdriver.common.action_chains import ActionChains
+import json
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.implicitly_wait(500)
+driver.set_window_size(1500,1000)
+# ===================================================================================================================================================================
+# 제이슨 파일로 저장
+def toJson(res_dict,save_name):
+    with open(f'{save_name}.json', 'w', encoding='utf-8') as file :
+        json.dump(res_dict, file, ensure_ascii=False, indent='\t')
+# ===================================================================================================================================================================
+
+# '더보기' 자동으로 누르기 함수
+# def more_main_page():
+#     more_css = "#reviews > div.cd-review__more > button"
+#     try:
+#       is_more = driver.find_element(By.CSS_SELECTOR, more_css).is_displayed()
+#     except Exception as e:
+#       is_more = False
+#     # true/ false
+
+#     while is_more:
+#         try:
+#             driver.find_element(
+#             By.CSS_SELECTOR,
+#             more_css
+#             ).send_keys(Keys.ENTER)
+#             time.sleep(2)
+#         except Exception as e:
+#             break
+
+
+# more_main_page()
+def run():
+  index = 0
+  youtuberDict = {}
+  for i in range(1,6):
+    driver.get(f"https://www.youha.info/search/influencers/youtube-channels?page={i}")
+    sleep(3)
+    youtubers = driver.find_elements(By.CSS_SELECTOR,'div.MuiDataGrid-row')
+    for youtuber in youtubers:
+      try:
+        imgUrl = youtuber.find_element(By.CSS_SELECTOR,'img.MuiAvatar-img.mui-style-1hy9t21').get_attribute('src').strip()
+        name = youtuber.find_element(By.CSS_SELECTOR,'p.MuiTypography-root.MuiTypography-body1.mui-style-1hxmejr').text.strip()
+        link = youtuber.find_element(By.CSS_SELECTOR,'div.MuiBox-root.mui-style-k008qs a').get_attribute('href').strip()
+        subscribers = youtuber.find_elements(By.CSS_SELECTOR,'div.MuiDataGrid-cell--withRenderer.MuiDataGrid-cell.MuiDataGrid-cell--textRight')[0].text.strip()
+        print(f"index : {index}, name : {name}, subscribers, {subscribers}, link : {link}, imgUrl : {imgUrl}")
+        youtuberDict[index] = {'name' : name, 'subscribers' : subscribers, 'link' : link, 'imgUrl' : imgUrl}
+        index += 1
+      except Exception as e:
+        continue
+  toJson(youtuberDict,f'youtuberData')
+    
+    
+
+
+    # page = 8
+
+
+    # reviewDict = {}
+    # driver.get(f"http://www.statiz.co.kr/stat.php")
+    # for i in range(1,25):
+    #   review_list = []
+    #   driver.find_element(
+    #     By.CSS_SELECTOR,
+    #     f"#courses_section > div > div > div > main > div.courses_container > div > div:nth-child({i}) > div > a"
+    #     ).send_keys(Keys.ENTER)
+
+    #   # more_main_page()
+    #   title = driver.find_element(By.CSS_SELECTOR,'h1.cd-header__title').text.rstrip('\n대시보드')
+    #   items = driver.find_elements(By.CSS_SELECTOR,'div.review-el__body')
+    #   for item in items[:300]:
+    #       try:
+    #           review_list.append(item.text.strip())
+    #       except Exception as e:
+    #           continue
+    #   print('title : ', title)
+    #   print('---------------------------------------------------')
+    #   print('review : ', review_list)
+    #   print('---------------------------------------------------')
+    #   reviewDict[title] = {'review' : review_list}
+    #   driver.back()
+    
+    # toJson(reviewDict,f'reviewData{page}')
